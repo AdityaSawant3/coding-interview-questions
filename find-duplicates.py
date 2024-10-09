618a12e92b52fc3c242fb9cc8bdc127039bb083d
AdityaSawant3 <sawant.aditya7766@gmail.com>
2024-10-09 20:17:08 +0530

    Implement find duplicate program

---
+++ find-duplicates.py
@@ -0,0 +1,19 @@
+class Find_Duplicates:
+
+	# 1st Approach:
+	# Time Complexity: O(n)
+	# Space Complexity: O(n)
+	def find_duplicates_using_set(self, list_items):
+		s = set()
+		list1 = []
+		for item in list_items:
+			if item in s:
+				list1.append(item)
+			else:
+				s.add(item)
+		print(list1)
+
+find_duplicate = Find_Duplicates()
+
+lists = [2, 3, 5, 2, 8, 34, 1, 23, 34, 5]
+find_duplicate.find_duplicates_using_set(lists)
