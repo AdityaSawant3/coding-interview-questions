7029d86b08c0d32c59062331056fc093a2c3a667
AdityaSawant3 <sawant.aditya7766@gmail.com>
2024-10-12 11:54:00 +0530

    Implement program to find minimum number from list with including -n...0.

--- find_min_num.py
+++ find_min_num.py
@@ -1,17 +1,17 @@
 # Time Complexity: O(n)
 # Space Complexity: O(1)
 def find_min(items):
-	min_num = 0
+	min_num = items[0]
 	for i in range(1, len(items)):
-		if items[i-1] < 0 or items[i] < 0:
+		if min_num <= 0 or items[i] <= 0:
 			pass
 		else:
-			if items[i-1] < items[i]:				
-				min_num = items[i-1]
+			if min_num < items[i]:				
+				pass
 			else:
 				min_num = items[i]
 	return min_num
 
-item_list = [6, 4, -2, 5, 1, 9, -7, 0]
+item_list = [6, 4, -2, 5, 9, -7, 0, 1]
 min_number = find_min(item_list)
 print(f"Minimum number in list is: {min_number}")
